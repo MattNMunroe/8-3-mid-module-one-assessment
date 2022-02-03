e5a1574d930d392d50b8770a36c8f76a525a9af2
Matt M <Afrodesiacc@gmail.com>
2022-02-03 12:50:43 -0500

    Finished; missing two tests

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Hello Myra/Tristan! I understand it is bad practice to leave comments with logic in the final submission, but I will be leaving mine in as I want feedback on if the pathing of my thoughts is correct more so than the code, if that makes sense. Should this be unacceptable, I will gladly resubmit a copy without them, thank you.
+
 /*
   You may use the following animals array to test your functions inside of this file. This is the same array of animals that is used in the tests.
   
@@ -14,6 +16,8 @@ const animals = [
   { kind: "Cat", count: 2 },
 ];
 
+//The above is an Array of Objects depicting a list of animals and their census. It contains a key with the name of "Kind" and another key with the name of "Count". To add a key:value in an object, one can use dot notation (animals.kind = 'Leopard').
+
 /**
  * FUNCTION DESCRIPTION
  * ------------------
@@ -26,7 +30,20 @@ const animals = [
  * getTotalCount(animals); //> 28
  * getTotalCount([]); //> 0 // returns 0 if the input array is empty
  */
-function getTotalCount(animals) {}
+function getTotalCount(animals) {
+//create a variable to house an originator value with which the count will be added to, should it exist.
+  let number = 0
+//I no value exists, it must return zero
+if (animals.count = 0) {
+  return 0
+}
+//I must loop through the array of objects, but only of the counts
+for (let i = 0; i < animals.length; i++) {
+  number += animals[i].count
+}
+  // must return a whole number, not an array
+  return number
+}
 
 /**
  * FUNCTION DESCRIPTION
@@ -40,7 +57,21 @@ function getTotalCount(animals) {}
  * getAllKinds(animals); //> ["Pig", "Cow", "Chicken", "Horse", "Dog", "Cat"]
  * getAllKinds([]); //> [] // returns empty array if input array is empty
  */
-function getAllKinds(animals) {}
+function getAllKinds(animals) {
+//The return must be an array of strings
+  let allAnimals = []
+//Must return an empty array if no input
+if (animals.kind === '') {
+  return allAnimals
+}
+//My loop must go thorugh each kind from the animals object
+for (let i = 0; i < animals.length; i++) {
+  allAnimals.push(animals[i].kind)
+}
+
+//Here I return my originally set array of strings, where the results will be placed
+return allAnimals
+}
 
 /**
  * FUNCTION DESCRIPTION
@@ -59,7 +90,21 @@ function getAllKinds(animals) {}
   ];
  * filterByCountMinimum([], 3); //> [] // returns empty array if input array is empty
  */
-function filterByCountMinimum(animals, minimum) {}
+function filterByCountMinimum(animals, minimum) {
+//creating a blank array for where our results will be inputed and as a default value in case no entry is submitted
+  let filteredCount = []
+//my loop must run, and begin to check for counts that are equal or greater than the amount. I must then push said results into my new array
+for (let i = 0; i < animals.length; i++) {
+// an if statement is needed to check the greater than or equal to requirement. We must refer to the count   
+  
+  if (animals[i].count >= minimum) {
+    filteredCount.push(animals[i])
+  }
+}
+
+//returns all objects
+return filteredCount
+}
 
 /**
  * FUNCTION DESCRIPTION
@@ -73,7 +118,15 @@ function filterByCountMinimum(animals, minimum) {}
  * getMostCommonAnimal(animals); //> { kind: "Chicken", count: 11 }
  * getMostCommonAnimal([]); //> null // returns null if the input is empty
  */
-function getMostCommonAnimal(animals) {}
+function getMostCommonAnimal(animals) {
+ // creating an empty object with which to push our results into and to account for if no entry is submitted 
+  let commonAnimal = null;
+  if (animals[0]) {
+  commonAnimal = animals[0]
+}
+//returning our object
+  return commonAnimal
+}
 
 // Do not change anything below this line.
 module.exports = {
